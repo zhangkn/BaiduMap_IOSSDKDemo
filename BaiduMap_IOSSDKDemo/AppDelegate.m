32cac28bfe90958568dc1fae6fc6d85a2ccbaf54
kevin <929118967@qq.com>
2016-10-07 16:47:59 +0800

    Hello BaiduMap

--- BaiduMap_IOSSDKDemo/AppDelegate.m
+++ BaiduMap_IOSSDKDemo/AppDelegate.m
@@ -16,7 +16,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // 要使用百度地图，请先启动BaiduMapManager
+    _mapManager = [[BMKMapManager alloc]init];
+    // 如果要关注网络及授权验证事件，请设定     generalDelegate参数
+    BOOL ret = [_mapManager start:@"FkQype3nIGnRf9nxpZP5bzeF3gxlVhrI"  generalDelegate:nil];
+    if (!ret) {
+        NSLog(@"manager start failed!");
+    }else{
+        NSLog(@"manager start success!");
+    }
+    
     return YES;
 }
 
--- BaiduMap_IOSSDKDemo/ViewController.m
+++ BaiduMap_IOSSDKDemo/ViewController.m
@@ -7,22 +7,48 @@
 //
 
 #import "ViewController.h"
+@interface ViewController ()<BMKMapViewDelegate>
 
-@interface ViewController ()
+@property (nonatomic,weak) BMKMapView *mapView;
 
 @end
 
 @implementation ViewController
 
+- (BMKMapView *)mapView{
+    if (nil == _mapView) {
+        BMKMapView *tmpView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, KMainScreenWidth, KMainScreenHeight)];
+        _mapView = tmpView;
+        self.view = tmpView;
+    }
+    return _mapView;
+}
+
+- (void)loadView{
+    [self mapView];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [_mapView viewWillAppear];
+    _mapView.delegate = self; // 此处记得不用的时候需要置nil，否则影响内存的释放
+}
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [_mapView viewWillDisappear];
+    _mapView.delegate = nil; // 不用时，置nil
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
